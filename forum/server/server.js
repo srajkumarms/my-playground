ee500923d5f0f193e9e444b8e7e94f5c574a7c8b
srajkumarms <srajkumarms@gmail.com>
2015-04-15 19:03:46 +0000

    Server modified with MongoDB connection

--- forum/server/server.js
+++ forum/server/server.js
@@ -1,16 +1,45 @@
 /* just adding a comment for desktop checkin */
 var http = require('http');
 var colors = require("colors");
-var mongo = require("mongoose");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://srajkumarms-my-playground-1392957:27017/test');
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<b>Hello World</b>');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("MongoDB test database is connected...".green);
+});
+
+
+var kittySchema = mongoose.Schema({
+    name: String
+});
+var Cat = mongoose.model('Cat', kittySchema);
+
+var server = http.createServer(function(request, response) {
   console.log('Hi, I got a request'.green);
+  var kitty = new Cat({
+    name: 'Zildjian'
+  });
+  kitty.save(function(err) {
+    if (err) // ...
+      console.log('meow');
+  });
+  response.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  //response.end('<b>Hello World</b>');
+  
+  Cat.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+    response.end(kittens.toString());
+  });
+  
 });
 server.listen(8080);
 
-console.log('Server running at ' + process.env.IP + ":"+ process.env.PORT);
+console.log('Server running at ' + process.env.IP + ":" + process.env.PORT);
 
 /*
 var port = 3000;
