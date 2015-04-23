6474a9e643ba2b634cc4306e313a61b6b7053d11
srajkumarms <srajkumarms@gmail.com>
2015-04-23 20:11:28 +0000

    Changed the server code

--- forum/server/server.js
+++ forum/server/server.js
@@ -1,59 +1,27 @@
-/* just adding a comment for desktop checkin */
-var http = require('http');
-var colors = require("colors");
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://srajkumarms-my-playground-1392957:27017/test');
+var admin = require('./modules/admin');
+var express = require('express');
+/* Main module */
+var app = express();
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log("MongoDB test database is connected...".green);
+/* Initialize Admin module */
+app.use('/admin', admin);
+
+/* Create virtual directories for our client content folders */
+app.use('/v', express.static('../client/views'));
+app.use('/s', express.static('../client/scripts'));
+app.use('/c', express.static('../client/styles'));
+app.use('/f', express.static('../client/fonts'));
+
+/* Initialize server and listen for requests */
+var server = app.listen(process.env.PORT, function () {
+
+  var host = process.env.IP;
+  var port = process.env.PORT;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
 
-
-var kittySchema = mongoose.Schema({
-    name: String
+app.get('/', function(request, response) {
+  response.redirect('/v/index.html#/');
 });
-var Cat = mongoose.model('Cat', kittySchema);
-
-var server = http.createServer(function(request, response) {
-  console.log('Hi, I got a request'.green);
-  var kitty = new Cat({
-    name: 'Zildjian'
-  });
-  kitty.save(function(err) {
-    if (err) // ...
-      console.log('meow');
-  });
-  response.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  //response.end('<b>Hello World</b>');
-  
-  Cat.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-    response.end(kittens.toString());
-  });
-  
-});
-server.listen(8080);
-
-console.log('Server running at ' + process.env.IP + ":" + process.env.PORT);
-
-/*
-var port = 3000;
-var https = require("https");
-var fs = require('fs');
-var path = require("path");
-
-var options = {
-  pfx: fs.readFileSync('./certificate/gotoforum.pfx')
-};
-
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
-console.log(("Server running in https://127.0.0.1:" + port).red);
-*/
